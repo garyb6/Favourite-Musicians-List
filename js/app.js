3919eb578f0b2340698699edeeffe7a49686aef9
garyb6 <gary.bennett6@outlook.com>
2022-01-07 17:41:18 +0000

    form entry adds to list and delete all button functions

--- js/app.js
+++ js/app.js
@@ -22,13 +22,23 @@ const createArtistListEntry = function (form){
     const artistListEntry = document.createElement('li');
     artistListEntry.classList.add('artist-list-item');
 
-    const artist = document.createElement('h3')
+    const artist = document.createElement('h3');
     artist.textContent = form.artist.value;
     artistListEntry.appendChild(artist);
 
-    const genre = document.createElement('h4')
+    const country = document.createElement('h4');
+    country.textContent = form.country.value;
+    artistListEntry.appendChild(country);
+
+    const genre = document.createElement('p');
     genre.textContent = form.genre.value;
-    artistListEntry.append
+    artistListEntry.appendChild(genre);
+
 
     return artistListEntry;
+}
+
+const handleDeleteAll = function (){
+    const artistList = document.querySelector('#artist-list');
+    artistList.innerHTML = null;
 }
